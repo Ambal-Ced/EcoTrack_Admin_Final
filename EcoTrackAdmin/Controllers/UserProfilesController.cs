f3da949801aa990813dd13ef5a4e547490751872
Ambal_Ced <justineambal57@gmail.com>
2024-10-31 13:29:53 +0800

    Add project files.

---
+++ EcoTrackAdmin/Controllers/UserProfilesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EcoTrackAdmin.Areas.Identity.Data;
+using EcoTrackAdmin.Models;
+
+namespace EcoTrackAdmin.Controllers
+{
+    public class UserProfilesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserProfilesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserProfiles
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.UserProfiles.Include(u => u.ApplicationUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: UserProfiles/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userProfile = await _context.UserProfiles
+                .Include(u => u.ApplicationUser)
+                .FirstOrDefaultAsync(m => m.UniqueUserName == id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Create
+        public IActionResult Create()
+        {
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: UserProfiles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UniqueUserName,ApplicationUserId")] UserProfile userProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userProfile);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Id", userProfile.ApplicationUserId);
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userProfile = await _context.UserProfiles.FindAsync(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Id", userProfile.ApplicationUserId);
+            return View(userProfile);
+        }
+
+        // POST: UserProfiles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("UniqueUserName,ApplicationUserId")] UserProfile userProfile)
+        {
+            if (id != userProfile.UniqueUserName)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userProfile);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserProfileExists(userProfile.UniqueUserName))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Id", userProfile.ApplicationUserId);
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userProfile = await _context.UserProfiles
+                .Include(u => u.ApplicationUser)
+                .FirstOrDefaultAsync(m => m.UniqueUserName == id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(userProfile);
+        }
+
+        // POST: UserProfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var userProfile = await _context.UserProfiles.FindAsync(id);
+            if (userProfile != null)
+            {
+                _context.UserProfiles.Remove(userProfile);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserProfileExists(string id)
+        {
+            return _context.UserProfiles.Any(e => e.UniqueUserName == id);
+        }
+    }
+}
---
+++ EcoTrackAdmin/Models/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using EcoTrackAdmin.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcoTrackAdmin.Models
+{
+    public class UserProfile
+    {
+        [Key]
+        [Required]
+        public string UniqueUserName { get; set; }
+
+        // Foreign key reference to ApplicationUser
+        [Required]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
